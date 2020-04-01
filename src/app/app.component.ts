58fa5df0b57c13487e66185d956d9fffb9706e86
MaximusAlvarez <nikhilr.v2.0@gmail.com>
2020-04-01 17:08:15 +0530

    made responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'my-website';
+  innerWidth: number;
+  above800: boolean = false;
+
   mouseenter($event){
     if($event.path[0].innerText === "--- Nikhil Rajesh ---")
       $event.path[0].innerText = "--- Batman ---"
@@ -18,5 +21,24 @@ export class AppComponent {
       $event.path[0].innerText = "--- Nikhil Rajesh ---"
     else 
       $event.path[0].innerText = "Student + Developer"
+  }
+
+  @HostListener("window:resize", ['$event'])
+  private onResize(event) {
+  }
+  ngAfterViewInit() {
+    this.detectScreenSize();
+  }
+
+  private detectScreenSize() {
+    if(this.innerWidth < 800)
+      this.above800 = false
+    else 
+      this.above800 = true
+    console.log(this.above800)
+  }
+
+  ngOnInit() {
+    this.innerWidth = window.innerWidth;
   }
 }
