7631092bca2f093ca5522a851b0db1507001b44b
Ph4t3 <nikhilr.v2.0@gmail.com>
2020-07-23 11:56:08 +0530

    added redpwn blog entry component

---
+++ src/app/blog-entries/redpwn-ctf2020/redpwn-ctf2020.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-redpwn-ctf2020',
+  templateUrl: './redpwn-ctf2020.component.html',
+  styleUrls: ['./redpwn-ctf2020.component.scss']
+})
+export class RedpwnCTF2020Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
