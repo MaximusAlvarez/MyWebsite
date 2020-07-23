80f8781eb03cca4985924bc569af208999e706a2
Ph4t3 <nikhilr.v2.0@gmail.com>
2020-07-23 11:56:27 +0530

    added not found component

---
+++ src/app/not-found/not-found.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { faExclamationTriangle, faNewspaper } from '@fortawesome/free-solid-svg-icons';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-not-found',
+  templateUrl: './not-found.component.html',
+  styleUrls: ['./not-found.component.scss']
+})
+export class NotFoundComponent implements OnInit {
+
+  constructor(private router: Router) { 
+    this.checkIfBlog();
+  }
+
+  blog: boolean;
+  faWarning = faExclamationTriangle;
+  faNewsPaper = faNewspaper;
+
+  checkIfBlog() {
+    const urlSplit = this.router.url.split('/');
+    this.blog = urlSplit[1] === 'blog';
+  }
+
+  ngOnInit(): void {
+  }
+
+}
